64808990793c390b8904eae54a68a018dcbad74b
ravikumar-darsi <ravikumardarsi90@gmail.com>
2024-09-13 16:26:32 +0530

    custom variables for making custom module to roboshop

---
+++ variables.tf
@@ -0,0 +1,48 @@
+variable "common_tags" {
+  # default = {
+  #   Project     = "roboshop"
+  #   Emvironment = "dev"
+  #   Terraform   = "true"
+  # }
+}
+
+variable "tags" {
+
+}
+
+variable "project_name" {
+  #default = "roboshop"
+}
+
+variable "environment" {
+  #default = "dev"
+}
+
+variable "zone_name" {
+  #default = "daws67s.online"
+}
+
+variable "vpc_id" {
+
+}
+
+variable "component_sg_id" {
+
+}
+
+variable "private_subnet_ids" {
+
+}
+
+variable "iam_instance_profile" {
+
+}
+
+variable "app_alb_listener_arn" {
+
+}
+
+variable "rule_priority" {
+
+}
+
