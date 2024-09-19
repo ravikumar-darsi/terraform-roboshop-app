b9cd5767b87c9d542cea0bbf8bfd6127e6a36f12
ravikumar-darsi <ravikumardarsi90@gmail.com>
2024-09-19 12:48:37 +0530

    added app_version in remote-exec block

--- main.tf
+++ main.tf
@@ -1,28 +1,28 @@
 resource "aws_lb_target_group" "component" {
-  name     = "${local.name}-${var.tags.Component}"
-  port     = 8080
-  protocol = "HTTP"
-  vpc_id   = var.vpc_id
+  name                 = "${local.name}-${var.tags.Component}"
+  port                 = 8080
+  protocol             = "HTTP"
+  vpc_id               = var.vpc_id
   deregistration_delay = 60
   health_check {
-      healthy_threshold   = 2
-      interval            = 10
-      unhealthy_threshold = 3
-      timeout             = 5
-      path                = "/health"
-      port                = 8080
-      matcher = "200-299"
+    healthy_threshold   = 2
+    interval            = 10
+    unhealthy_threshold = 3
+    timeout             = 5
+    path                = "/health"
+    port                = 8080
+    matcher             = "200-299"
   }
 }
 
 module "component" {
   source                 = "terraform-aws-modules/ec2-instance/aws"
-  ami = data.aws_ami.centos8.id
+  ami                    = data.aws_ami.centos8.id
   name                   = "${local.name}-${var.tags.Component}-ami"
   instance_type          = "t2.micro"
   vpc_security_group_ids = [var.component_sg_id]
   #subnet_id              = element(split(",", data.aws_ssm_parameter.private_subnet_ids.value), 0)
-  subnet_id = element(var.private_subnet_ids, 0)
+  subnet_id            = element(var.private_subnet_ids, 0)
   iam_instance_profile = var.iam_instance_profile
   tags = merge(
     var.common_tags,
@@ -39,9 +39,9 @@ resource "null_resource" "component" {
   # Bootstrap script can run on any instance of the cluster
   # So we just choose the first in this case
   connection {
-    host = module.component.private_ip
-    type = "ssh"
-    user = "centos"
+    host     = module.component.private_ip
+    type     = "ssh"
+    user     = "centos"
     password = "DevOps321"
   }
 
@@ -54,7 +54,7 @@ resource "null_resource" "component" {
     # Bootstrap script called with private_ip of each node in the cluster
     inline = [
       "chmod +x /tmp/bootstrap.sh",
-      "sudo sh /tmp/bootstrap.sh ${var.tags.Component} ${var.environment}"
+      "sudo sh /tmp/bootstrap.sh ${var.tags.Component} ${var.environment} ${var.app_version}"
     ]
   }
 }
@@ -62,13 +62,13 @@ resource "null_resource" "component" {
 resource "aws_ec2_instance_state" "component" {
   instance_id = module.component.id
   state       = "stopped"
-  depends_on = [ null_resource.component ]
+  depends_on  = [null_resource.component]
 }
 
 resource "aws_ami_from_instance" "component" {
   name               = "${local.name}-${var.tags.Component}-${local.current_time}"
   source_instance_id = module.component.id
-  depends_on = [ aws_ec2_instance_state.component ]
+  depends_on         = [aws_ec2_instance_state.component]
 }
 
 resource "null_resource" "component_delete" {
@@ -82,16 +82,16 @@ resource "null_resource" "component_delete" {
     command = "aws ec2 terminate-instances --instance-ids ${module.component.id}"
   }
 
-  depends_on = [ aws_ami_from_instance.component]
+  depends_on = [aws_ami_from_instance.component]
 }
 
 resource "aws_launch_template" "component" {
   name = "${local.name}-${var.tags.Component}"
 
-  image_id = aws_ami_from_instance.component.id
+  image_id                             = aws_ami_from_instance.component.id
   instance_initiated_shutdown_behavior = "terminate"
-  instance_type = "t2.micro"
-  update_default_version = true
+  instance_type                        = "t2.micro"
+  update_default_version               = true
 
   vpc_security_group_ids = [var.component_sg_id]
 
@@ -113,8 +113,8 @@ resource "aws_autoscaling_group" "component" {
   health_check_type         = "ELB"
   desired_capacity          = 2
   vpc_zone_identifier       = var.private_subnet_ids
-  target_group_arns = [ aws_lb_target_group.component.arn ]
-  
+  target_group_arns         = [aws_lb_target_group.component.arn]
+
   launch_template {
     id      = aws_launch_template.component.id
     version = aws_launch_template.component.latest_version
